d854460ce3eb069cf68e21acba2b565cd3e81149
aprendendohenrique <henriquericardofodi@gmail.com>
2026-04-30 11:24:24 -0300

    Exercise: Make another visualization, but with cube numbers

--- mpl_cubes.py
+++ mpl_cubes.py
@@ -1,3 +1,17 @@
+from turtledemo.chaos import plot
+
 import matplotlib.pyplot as plt
 
 
+x_values = range(1, 5001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use("Solarize_Light2")
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.Greens)
+
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Values", fontsize=14)
+ax.set_ylabel("Cube Numbers", fontsize=14)
+
+plt.show()
