7aedeb3361acb9f392b9cec80032f492e76b3ce9
aprendendohenrique <henriquericardofodi@gmail.com>
2026-05-01 11:46:31 -0300

    Updates the random_walk.py and rw_visal.py files

--- rw_visual.py
+++ rw_visual.py
@@ -3,13 +3,29 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a rangom walk.
+    rw = RandomWalk(500000)
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect("equal")
-plt.show()
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    # ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=0.1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=0.05)
+    ax.set_aspect("equal")
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=25)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=25)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
